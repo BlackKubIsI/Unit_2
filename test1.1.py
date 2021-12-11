cbf3dd56e18d0f9c2a9500f2a9b7315a68e9d871
BlackKubIsI <xatakekakasi170@gmail.com>
2021-12-11 16:19:14 +0300

    123

--- test1.1.py
+++ test1.1.py
@@ -2,10 +2,10 @@ print("1.1")
 print("1.2")
 print("1.3")
 print(list("YYYYYYYYYYYYYYYY"))
-
-
 print("2.1")
 print("2.2")
 print("2.3")
 print("2.4")
 print(list("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"))
+
+print("NEW_!__> 2")
